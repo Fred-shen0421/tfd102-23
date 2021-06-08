2a5507e07bb1f0e90bbe42dd0156b399b01009a4
shen0421 <fred870421@gmail.com>
2021-06-09 05:21:54 +0800

    chore : update rwd

--- js/allproduct.js
+++ js/allproduct.js
@@ -1,13 +1,13 @@
-let buy = document.querySelector('.shop-cart');
-let shop = document.querySelector('.shopping-car');
-let back = document.querySelector('body');
+// let buy = document.querySelector('.shop-cart');
+// let shop = document.querySelector('.shopping-car');
+// let back = document.querySelector('body');
 
-buy.addEventListener('click', function (e) {
-  e.stopPropagation();
-  shop.style.left = '0px';
-});
+// buy.addEventListener('click', function (e) {
+//   e.stopPropagation();
+//   shop.style.left = '0px';
+// });
 
-back.addEventListener('click', function () {
-  shop.style.left = '-300px';
-  console.log('hi');
-});
+// back.addEventListener('click', function () {
+//   shop.style.left = '-300px';
+//   console.log('hi');
+// });
