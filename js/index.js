8b61c311927a1f57e9b0711e1586ff081cc34a6f
shen0421 <fred870421@gmail.com>
2021-06-08 22:29:15 +0800

    chore : change login function and update rwd

--- js/index.js
+++ js/index.js
@@ -89,19 +89,9 @@ document.addEventListener('scroll', function () {
 
 // login
 
-const signinbtn = document.querySelector('.signinbtn');
-const signupbtn = document.querySelector('.signupbtn');
-const formbx = document.querySelector('.formbx');
-const body = document.querySelector('body');
-
-signupbtn.onclick = function () {
-  formbx.classList.add('active');
-  // body.classList.add('active');
-};
-
-signinbtn.onclick = function () {
-  formbx.classList.remove('active');
-  // body.classList.remove('active');
+const toggleForm = () => {
+  const contain = document.querySelector('.contain');
+  contain.classList.toggle('active');
 };
 
 let clicklogin = document.querySelector('.click-login');
